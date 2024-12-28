301b00fb3c8832d6cf0abb845667bb844035c599
gargdivyansh1 <divyanshgarg515@gmail.com>
2024-12-28 22:48:20 +0530

    util code discussion

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,24 @@
+// lets do it with the use of promised 
+// resolve reject or the resolve catch could also work
+const asynchandler = (requesthandler) => (req,res,next) => {
+    Promise.resolve(requesthandler(req,res,next))
+    .catch((error) => next(error))
+}
+
+export {asynchandler}
+
+
+// this is where we are doing with the try catch syntax 
+
+// these are higher order functions 
+// these can accept the fucctyion and also can pass it as response
+// const asynchandler = (fn) => async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
