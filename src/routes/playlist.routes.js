dae6c9fc501e9a0c4b40b27b1839ebc0e83dafcf
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-01-10 10:30:42 +0530

    add playlist routes: implement additional routes for managing user playlists

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -5,11 +5,17 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/create-playlist").post(createPlaylist)
+
 router.route("/get-user-playlist/:userId").get(verifyJWT , getUserPlaylists)
+
 router.route("/get-playlist-By-id/:playlistId").get(verifyJWT , getPlaylistById)
+
 router.route("/add-video/:playlistId/:videoId").post(verifyJWT, addVideoToPlaylist)
+
 router.route("/remove-video/:playlistId/:videoId").patch(verifyJWT, removeVideoFromPlaylist)
+
 router.route("/delete-playlist/:playlistId").delete(verifyJWT , deletePlaylist)
+
 router.route("/update-playlist/:playlistId").post(verifyJWT,
     updatePlaylist
 )
