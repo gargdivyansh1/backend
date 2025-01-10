b59d76cc9d421b2e53b5bc9b478a1c9b9099d95b
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-01-10 11:37:56 +0530

    add video routes: implement additional routes for adding, updating, deleting, and toggling video publish status

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,6 +1,14 @@
 import Router from 'express'
-import { getAllVideos } from '../controllers/video.controllers'
+import { AddAVideo, deleteVideo, getAllVideos, getvideosById, togglePublishStatus, updateVideo } from '../controllers/video.controllers.js'
+import {verifyJWt} from '../middlewares/auth.middleware.js'
 
-const router = router()
+const router = Router()
 
 router.route('/videos').get(getAllVideos)
+router.route('/add-video').post(AddAVideo)
+router.route("/get-video-By-Id/:{videoId").get(verifyJWt , getvideosById)
+router.route("/update-video/:videoId").patch(verifyJWt, updateVideo)
+router.route("/delete-video/:videoId").delete(verifyJWt, deleteVideo)
+router.route("/toggle-video/:{videoId").patch(verifyJWt, togglePublishStatus)
+
+export default router
